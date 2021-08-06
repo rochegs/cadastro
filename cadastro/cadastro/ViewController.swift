dca48e64c04b973037a4e9b4b7ea6062faad695e
thiagoleite6 <thiago.0686@gmail.com>
2021-08-06 14:37:51 -0300

    Teste de push na origin

--- cadastro/cadastro/ViewController.swift
+++ cadastro/cadastro/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
